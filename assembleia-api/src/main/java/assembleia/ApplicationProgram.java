991ae64818e86022fe8223fc0b1f63ec94c1c810
Rodrigo Duarte <rodrigo.duarte@totvs.com.br>
2020-10-04 20:24:51 -0300

    Ajuste no pom para tentativa de deploy no heroku

--- assembleia-api/src/main/java/assembleia/ApplicationProgram.java
+++ assembleia-api/src/main/java/assembleia/ApplicationProgram.java
@@ -2,9 +2,10 @@ package assembleia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationProgram {
+public class ApplicationProgram extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationProgram.class, args);
